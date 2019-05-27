671f12d14cf5506626be15c81a500d87331cda81
Vladimir Spirin <spirin.vladimir@gmail.com>
2019-05-27 10:15:22 +0300

    test coverage 100%

--- test.js
+++ test.js
@@ -1,25 +1,35 @@
 var
     assert = require('assert'),
-    {signal, map} = require('./index')
+    {signal, map, combine} = require('./index')
 
 describe('signal', () => {
-    it('should stream from [1, 2, 3] to [1, 2, 3]', () => {
+    it('should stream from [1, 2, 3] to [1, 2, 3]', done => {
         var
             from = [1, 2, 3],
             to = []
 
-        signal(emit => from.forEach(_ => setTimeout(() => emit(_), _)))
-            .onValue(value =>
-                to.push(value) === from.length && assert.deepEqual(from, to))
+        signal(emit => from.forEach(_ => setTimeout(emit, 0, _)))
+            .onValue(value => {
+                to.push(value)
+                if (to.length === from.length) {
+                    assert.deepEqual(from, to)
+                    done()
+                }
+            })
     })
-    it('should not call onValue if value is same', () => {
+    it('should not call onValue if value is same', done => {
         var
             from = [1, 2, 2, 3, 3, 3],
             to = []
 
-        signal(emit => from.forEach(_ => setTimeout(() => emit(_), _)))
-            .onValue(value =>
-                to.push(value) === 3 && assert.deepEqual(to, [1, 2, 3]))
+        signal(emit => from.forEach(_ => setTimeout(emit, 0, _)))
+            .onValue(value => {
+                to.push(value)
+                if (to.length === 3) {
+                    assert.deepEqual(to, [1, 2, 3])
+                    done()
+                }
+            })
     })
     it('subscribe on value asyc after create signal', done => {
         var
@@ -27,22 +37,74 @@ describe('signal', () => {
             to = [],
             time_scale = 10
 
-        var signal$ = signal(emit => from.forEach(_ => setTimeout(() => emit(_), _ * time_scale)))
+        var signal$ = signal(emit => from.forEach(_ => setTimeout(emit, _ * time_scale, _)))
         setTimeout(
             () =>
-                signal$.onValue(value =>
-                    to.push(value) === from.length && assert.deepEqual(to, from) || done()),
+                signal$.onValue(value => {
+                    to.push(value)
+                    if (to.length === from.length) {
+                        assert.deepEqual(to, from)
+                        done()
+                    }
+                }),
             time_scale * (from[0] + from[1]) / 2
         )
     })
-    it('map between signals chain', () => {
+    it('map between signals chain', done => {
         var
+            from = [1, 2, 3],
             to = []
 
         map(
-            signal(emit => [1, 2, 3].forEach(emit)),
+            signal(emit => [1, 2, 3].forEach(_ => setTimeout(emit, 0, _))),
             (value, emit) => emit(value + 1)
         )
-            .onValue(value => to.push(value) === 3 && assert.deepEqual(to, [2, 3, 4]))
+            .onValue(value => {
+                to.push(value)
+                if (to.length === from.length) {
+                    assert.deepEqual(to, [2, 3, 4])
+                    done()
+                }
+            })
+    })
+    it('async map between signals chain', done => {
+        var
+            from = [1, 2, 3],
+            to = [],
+            time_scale = 10
+
+        map(
+            signal(emit => from.forEach(_ => setTimeout(emit, 0, _))),
+            (value, emit) => setTimeout(emit, value * time_scale, value + 1)
+        )
+            .onValue(value => {
+                to.push(value)
+                if (to.length === from.length) {
+                    assert.deepEqual(to, [2, 3, 4])
+                    done()
+                }
+            })
+    })
+    it('combine', done => {
+        var
+            from1 = [1, 3, 5],
+            from2 = [2, 4, 6],
+            to = [],
+            t = 10
+
+        combine(
+            [
+                signal(emit => from1.forEach(_ => setTimeout(emit, _ * t, _))),
+                signal(emit => from2.forEach(_ => setTimeout(emit, _ * t, _))),
+            ],
+            (a, b) => Math.max(a || -Infinity, b || -Infinity)
+        )
+            .onValue(value => {
+                to.push(value)
+                if (to.length === from1.length + from2.length) {
+                    assert.deepEqual(to, [1, 2, 3, 4, 5, 6])
+                    done()
+                }
+            })
     })
 })
